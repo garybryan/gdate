89eb29154497b93244eab743ab00837d8694c844
Gary Bryan <garybryan@gmail.com>
2022-09-03 15:34:55 +0100

    Signup button

--- apps/frontend/components/onboarding-form/onboarding-form.tsx
+++ apps/frontend/components/onboarding-form/onboarding-form.tsx
@@ -1,5 +1,8 @@
 import MultipleChoiceField from '../multiple-choice-field/multiple-choice-field';
 import SingleChoiceField from '../single-choice-field/single-choice-field';
+
+import SexyButton from '../sexy-button/sexy-button';
+
 import styles from './onboarding-form.module.css';
 
 const interestedInChoices = [
@@ -19,16 +22,22 @@ const pineappleChoices = ['Love it! 🍍🍕🤤', 'No way! 🍍🍕🤮'];
 
 export function OnboardingForm() {
   return (
-    <div className={styles['container']}>
+    <div>
       <SingleChoiceField
         label="I'm interested in:"
         choices={interestedInChoices}
       />
+
       <MultipleChoiceField label="I'm into:" choices={interestsChoices} />
+
       <SingleChoiceField
         label="Does pineapple belong on pizza?"
         choices={pineappleChoices}
       />
+
+      <div className={styles['buttonContainer']}>
+        <SexyButton variant="contained" size="large" className={styles['signupButton']}>Sign me up!</SexyButton>
+      </div>
     </div>
   );
 }
---
+++ apps/frontend/components/sexy-button/sexy-button.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const SexyButton = styled(Button)({
+  backgroundColor: '#eb348c',
+  fontSize: '110%',
+});
+
+export default SexyButton;
