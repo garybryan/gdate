9a7219696a233f3792adedb45cef6f406516d72d
Gary Bryan <garybryan@gmail.com>
2022-09-04 19:26:56 +0100

    fix[frontend]: restore lost heading

--- apps/frontend/components/header/header.tsx
+++ apps/frontend/components/header/header.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.css';
 export function Header() {
   return (
     <header className={styles['header']}>
-      <h1>gdate</h1>
+      <h1 className={styles['headerText']}>gdate</h1>
     </header>
   );
 }
