019457c5486d83c950be043e6683f702335008ae
Gary Bryan <garybryan@gmail.com>
2022-09-06 16:39:20 +0100

    feat[frontend]: splash image and global background

--- apps/frontend/components/splash/splash.tsx
+++ apps/frontend/components/splash/splash.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import Image from 'next/image';
+
 import SexyButton from '../../components/sexy-button/sexy-button';
 
 import styles from './splash.module.css';
@@ -7,6 +9,13 @@ import styles from './splash.module.css';
 export function Splash() {
   return (
     <div className={styles['container']}>
+      <Image
+        alt="Happy couple"
+        src="/images/couple.jpg"
+        layout="fill"
+        objectFit="contain"
+        priority
+      />
       <SexyButton className={styles['button']}>Log in</SexyButton>
       <Link href="/onboarding" passHref>
         <SexyButton className={styles['button']}>Sign up</SexyButton>
