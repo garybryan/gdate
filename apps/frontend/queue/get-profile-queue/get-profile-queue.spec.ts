d64db32d16671c736e7927d331f732f1ef5b3dce
Gary Bryan <garybryan@gmail.com>
2022-09-06 15:54:06 +0100

    test[frontend]: attractiveness order test (deliberately failing for demo)

--- apps/frontend/queue/get-profile-queue/get-profile-queue.spec.ts
+++ apps/frontend/queue/get-profile-queue/get-profile-queue.spec.ts
@@ -1,24 +1,8 @@
 import getProfileQueue from './get-profile-queue';
 
 describe('getProfileQueue', () => {
-  it('gets queue of profiles', async () => {
+  it('gets profiles in order of attractiveness', async () => {
     const expectedProfileQueue = [
-      {
-        id: '185e62e9-1303-4f49-8056-8b40b0b35bf8',
-        name: 'Some loser',
-        photo: 'richard.jpg',
-        age: 29,
-        height: 175,
-        income: -20,
-      },
-      {
-        id: 'b8bc8926-85c8-4107-a802-403dbfa439a0',
-        name: 'Elrich',
-        photo: 'elrich.jpg',
-        age: 37,
-        height: 180,
-        income: 50,
-      },
       {
         id: 'f35280d1-499d-41c8-b395-fdbe4f4a5d56',
         name: 'Gary',
@@ -34,6 +18,22 @@ describe('getProfileQueue', () => {
         age: 33,
         height: 170,
         income: 200,
+      },
+      {
+        id: 'b8bc8926-85c8-4107-a802-403dbfa439a0',
+        name: 'Elrich',
+        photo: 'elrich.jpg',
+        age: 37,
+        height: 180,
+        income: 50,
+      },
+      {
+        id: '185e62e9-1303-4f49-8056-8b40b0b35bf8',
+        name: 'Some loser',
+        photo: 'richard.jpg',
+        age: 29,
+        height: 175,
+        income: -20,
       },
     ];
 
