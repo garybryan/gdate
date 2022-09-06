91d7cab6ed55091df5b87b8e1c7e1b0e7ea468ce
Gary Bryan <gary.bryan@lloydsbanking.com>
2022-09-06 18:07:42 +0100

    Reliable splash image

--- apps/frontend/components/splash/splash.tsx
+++ apps/frontend/components/splash/splash.tsx
@@ -1,7 +1,5 @@
 import Link from 'next/link';
 
-import Image from 'next/image';
-
 import SexyButton from '../../components/sexy-button/sexy-button';
 
 import styles from './splash.module.css';
@@ -9,13 +7,6 @@ import styles from './splash.module.css';
 export function Splash() {
   return (
     <div className={styles['container']}>
-      <Image
-        alt="Happy couple"
-        src="/images/couple.jpg"
-        layout="fill"
-        objectFit="contain"
-        priority
-      />
       <SexyButton className={styles['button']}>Log in</SexyButton>
       <Link href="/onboarding" passHref>
         <SexyButton className={styles['button']}>Sign up</SexyButton>
